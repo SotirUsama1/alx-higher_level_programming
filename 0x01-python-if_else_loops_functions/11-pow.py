2e27c0b80256b057120c5c00eae3aed2c5b77322
SotirUsama1 <sotirusama16@gmail.com>
2023-11-29 07:44:11 +0200

    task11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
     value = 1
-    for i in range (0, b):
-        value *= a
+    for i in range(0, b):
+        if b > 0:
+            value *= a
+        elif b < 0:
+            value /= a
+        elif b = 0
+            return 1
     return value
