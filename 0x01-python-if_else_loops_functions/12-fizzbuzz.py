6ea3c95e4476dd46ba188c332c75126a93ddce83
SotirUsama1 <sotirusama16@gmail.com>
2023-11-29 07:56:24 +0200

    task12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    temp = ""
+    for i in range(1, 100):
+        if i % 3 == 0 and i % 5 == 0:
+            temp += "FizzBuzz "
+        elif i % 3 == 0:
+            temp += "Fizz "
+        elif i % 5 == 0:
+            temp += "Buzz "
+        else:
+            temp += "{} ".format(i)
+    temp += "Buzz"
+    print(temp)
