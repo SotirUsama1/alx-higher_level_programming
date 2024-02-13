d8a3a40983e00cd560a54e031d3eb2ab30ff1e02
Sotir Latif <sotirusama13@gmail.com>
2024-02-14 00:19:14 +0200

    Task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    args = argv
+    argc = len(args)
+    sum = 0
+    for i in range(1, argc):
+        sum = sum + int(args[i])
+    print(sum)
