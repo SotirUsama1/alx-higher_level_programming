32a0e3c5a02a342c267bfe27abd9daa718739ea1
SotirUsama1 <sotirusama16@gmail.com>
2024-02-28 14:15:56 +0200

    task1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     size = len(my_list)
     if idx < 0:
         return "none"
-    elif idx >= size;
+    elif idx >= size:
         return "none"
     else:
         return my_list.index(idx)
