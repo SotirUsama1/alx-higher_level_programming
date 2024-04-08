81b26e2d8c4e57d8bd095e8233894b741704166b
Sotir Latif <sotirusama13@gmail.com>
2024-04-08 21:53:18 +0200

    task 3 update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,3 +1,4 @@
+'''
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
@@ -7,3 +8,14 @@ if __name__ == "__main__":
     for i in range(1, argc):
         sum = sum + int(args[i])
     print(sum)
+'''
+
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
