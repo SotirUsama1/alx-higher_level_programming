e99b204af1651abf0a5a485706994f714e11b7a0
Sotir Latif <sotirusama13@gmail.com>
2024-04-10 20:02:23 +0200

    Task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    common = set_1 & set_2
-    uni = set_1.union(set_2)
-    unq = []
-    for i in uni:
-        for j in common:
-            if i == j:
-                break
-            unq.append(i)
-    return set(unq)
+    return set_1 ^ set_2
