d7b7d76fc59df1b6d6f76ad4f329b20e683660f2
Sotir Latif <sotirusama16@gmail.com>
2024-04-14 06:23:13 +0200

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None: return None
+    if a_dictionary is None:
+        return None
     my_list = [(k, a_dictionary.get(k)) for k in a_dictionary.keys()]
     best_s = 0
     best_k = ""
