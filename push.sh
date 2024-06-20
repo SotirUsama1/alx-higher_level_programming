a5c23f137fb8a40be227408ece0750937d39f923
SotirUsama1 <sotirusama16@gmail.com>
2024-06-20 04:46:50 +0300

    fix

--- push.sh
+++ push.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-echo 'commit: '
-read x
-git add ${PWD}
-git commit -m ${x}
+
+# Check if the first argument is empty
+if [ -z "$1" ]; then
+    echo 'commit: '
+    read x
+else
+    x=$1
+fi
+
+# Add current directory to git
+git add "${PWD}"
+
+# Commit with the message
+git commit -m "${x}"
+
+# Push the commit
 git push
