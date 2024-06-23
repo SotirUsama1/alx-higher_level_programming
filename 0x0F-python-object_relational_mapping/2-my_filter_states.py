0a76bf161a2ec1f786795685db234c543153ca5a
Sotir Latif <sotirusama16@gmail.com>
2024-06-23 20:58:11 +0300

    Fixes

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,12 +15,13 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name like \""
-                + sys.argv[4] + "\"")
+    if sys.argv[4]:
+        cur.execute("SELECT * FROM states WHERE name like \""
+                    + sys.argv[4] + "\"")
 
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
     cur.close()
     db.close()
