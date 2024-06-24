ca540f05c787a16a4a6639611a479ce20a85e052
Sotir Latif <sotirusama16@gmail.com>
2024-06-24 19:01:50 +0300

    fix 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     session = Session()
     new_state = State(name='Louisiana')
     session.add(new_state)
+    session.commit()
     new_instance = session.query(State).filter_by(name='Louisiana').first()
     print(new_instance.id)
-    session.commit()
